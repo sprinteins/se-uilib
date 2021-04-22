11e9064b2d0390e0a2ecb97d3112227b9ec9ddc2
Tamás Rusz <t.rusz@sprinteins.com>
2021-04-22 22:31:16 +0200

    add storybook

---
+++ ui-lib/src/components/button/button.stories.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+    import Button from './Button.svelte';
+  </script>
+  
+  <Meta title="Example/Button" component={Button}/>
+  
+  <Template let:args>
+    <Button {...args}/>
+  </Template>
+  
+  <Story name="Primary" args={{
+      primary: true,
+      label:"Primary",
+}}/>
+  
+  <Story name="Secondary" args={{
+      primary: false,
+      label:"Normal",
+}}/>
