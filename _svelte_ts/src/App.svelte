2fbc28e3e64eaeb7abc277e1772b0953b26a9f24
Tamás Rusz <t.rusz@sprinteins.com>
2021-04-23 23:24:48 +0200

    svelte ts with assets

---
+++ _svelte_ts/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style lang="scss" src="./app.scss">
+	
+</style>
