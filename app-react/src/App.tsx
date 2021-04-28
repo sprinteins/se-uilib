b699097de03dc3bd6127ccb549f9733a53f78abe
Tamás Rusz <t.rusz@sprinteins.com>
2021-04-28 22:35:02 +0200

    integrate uilib into react app

--- app-react/src/App.tsx
+++ app-react/src/App.tsx
@@ -1,43 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// @ts-ignore
-import {adapt} from "webcomponents-with-react"
 
-import '@dp/ui-lib'
+import {adapt} from "./adapter";
+
+import 'dpdhl-ui-lib';
 
 function App() {
 
+    const user = {name:'John'}
+    const ThemeDPDHL = adapt('theme-dpdhl');
+    const DPDHLButton = adapt('dpdhl-button');
 
-  const user = {name:'John'}
-  const HelloWorld = adapt('hello-world');
-
-  function handleOnClick(){
-    alert('Hello, World!');
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React?
-        </a>
-      </header>
-
-      <HelloWorld user={user} />
-
-    </div>
+    const [log, setLog] = useState('')
     
-  );
-}
+    function handleOnClick(){
+        alert('Hello, World!');
+    }
+    
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            <h1>React App</h1>
 
-export default App;
+
+            </header>
+            
+            <ThemeDPDHL>
+                <DPDHLButton 
+                    onClick={ () => setLog('Button Clicked') }
+                    primary="true"
+                    size="normal"
+                >
+                    Primary Button
+                </DPDHLButton>
+            </ThemeDPDHL>
+
+            <div>
+                {log}
+            </div>
+      
+        
+        </div>
+        
+        );
+    }
+    
+    export default App;
+    
