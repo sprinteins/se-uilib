0a0b215864c91acb53fcba65a9c34a3bf3b9039f
Tamás Rusz <t.rusz@sprinteins.com>
2021-05-03 22:49:08 +0200

    remove experiment

--- _composition/src/button.svelte
+++
@@ -1,3 +0,0 @@
-<svelte:options tag="x-button" />
-
-<button>click: <slot>no content</slot></button>
--- _composition/src/main.js
+++
@@ -1,8 +0,0 @@
-// import App from './App.svelte';
-import Card from './card.svelte'
-import ContactCard from './contact-card.svelte'
-
-
-// const app = new App({});
-
-// export default app;
