3ce16de4cac5d5797609ba8308288ae0812dabb0
Tamás Rusz <t.rusz@sprinteins.com>
2021-05-16 15:28:16 +0200

    add textarea

---
+++ uilib/uilib/src/components/dpdhl-input/dpdhl-textarea.stories.svelte
@@ -0,0 +1,81 @@
+<script>
+    import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+    import { action } from '@storybook/addon-actions';
+    
+    import 'dpdhl-uilib'
+
+
+</script>
+
+
+<Meta 
+    title="Components/Input" 
+    component={null}
+    argTypes={{
+        width: {
+            name: 'Width [px]',
+            control: { 
+                type: 'range', 
+                min: 100,
+                max: 500,
+                step: 10,
+            },
+        },
+        height: {
+            name: 'Height [px]',
+            control: { 
+                type: 'range', 
+                min: 100,
+                max: 500,
+                step: 10
+            },
+        },
+    }}
+/>
+
+<Template let:args>
+    <p>
+        <dpdhl-textarea 
+            placeholder={args.placeholder}
+            label={args.label}
+            rows={args.rows}
+            cols={args.cols}
+            value={args.value}
+            width={args.width}
+            height={args.height}
+            description={args.description}
+        />
+    </p>
+    <p>
+        <dpdhl-textarea 
+            disabled
+            placeholder={args.placeholder}
+            label={args.label}
+            rows={args.rows}
+            cols={args.cols}
+            value={args.value}
+            width={args.width}
+            height={args.height}
+            description={args.description}
+        />
+    </p>
+</Template>
+
+<style>
+    
+</style>
+
+<Story name="Text Area" args={{
+    label: 'Shipping Code',
+    placeholder: 'Ab34-5',
+    value: [
+        'Sunt iure quia saepe. Earum ut vel odio. Sapiente mollitia ut ',
+        'suscipit alias. Vel iusto ducimus nobis qui quo quia ullam. ',
+        'Atque esse delectus magni tenetur doloremque dolorem debitis. ',
+        'Reiciendis eius hic sint ut animi.',
+    ].join(),
+    description: 'Form field description text.',
+    width: 300,
+    height: 100,
+}}/>
+
---
+++ uilib/uilib/src/components/dpdhl-input/dpdhl-textarea.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    export let label = '';
+    export let placeholder = '';
+    export let disabled;
+    export let value = '';
+    export let width: number;
+    export let height: number;
+    export let description = '';
+
+    $: _disabled = disabled !== undefined
+</script>
+
+<svelte:options tag="dpdhl-textarea" />
+
+<div class="container">
+    <label>
+        <span class="label">{label}</span>
+        <textarea 
+            style="--width:{width}px; --height:{height}px;"
+            {value}
+            {width}
+            {height}
+            disabled={_disabled} 
+            {placeholder}
+        ></textarea>
+    </label>
+    
+    {#if description}
+        <span class="description">{description}</span>
+    {/if}
+</div>
+
+<style>
+    .container{
+        position: relative
+    }
+    .label{
+        position: absolute;
+        top:      1em;
+        left:     1.25em;
+        
+        font-size:        0.5em;
+        padding:          0.2em;
+        color:            var(--color-gray45);
+        background-color: var(--color-white);
+    }
+    .description{
+        font-size:        0.75em;
+        padding:          0.2em;
+        color:            var(--color-gray45);
+        background-color: var(--color-white);
+    }
+    textarea{
+        width: var(--width, 400px);
+        height: var(--height, 400px);
+
+        padding-top:    1.5em;
+        padding-left:   0.75em;
+        padding-bottom: 0.75em;
+        border-radius:  var(--border-radius);
+        border:         1px solid var(--color-gray45);
+    }
+    textarea:hover{
+        border: 1px solid var(--color-black);
+    }
+    textarea:active,
+    textarea:focus, 
+    textarea:not(:disabled):focus-visible {
+        border-color: var(--color-black);
+        box-shadow:   inset 0px 0px 0px 1px var(--color-black);
+        outline:      none;
+    }
+
+    textarea:disabled {
+        border-color:     var(--color-gray30);
+        background-color: unset;
+    }
+</style>
