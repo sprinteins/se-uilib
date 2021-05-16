508db76a6175c7b68a3fc4216684f6e39c1d6baf
Tamás Rusz <t.rusz@sprinteins.com>
2021-05-16 15:29:05 +0200

    uses svg icons

--- uilib/uilib/src/components/dpdhl-icon/dpdhl-icon.stories.svelte
+++ uilib/uilib/src/components/dpdhl-icon/dpdhl-icon.stories.svelte
@@ -1,21 +1,27 @@
 <script>
     import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
-    import { action } from '@storybook/addon-actions';
     
     import 'dpdhl-uilib'
-
-
 </script>
 
 
 <Meta title="Components/Icon" component={null}/>
 
+<!-- https://www.dpdhl-brands.com/dpdhl-group/en/tools/icon-library.html -->
 <Template let:args>
-   <dpdhl-icon icon="MoreVertical" />
+   <dpdhl-icon width={args.width} height={args.height} icon="arrow_back" />
+   <dpdhl-icon width={args.width} height={args.height} icon="plus_circle" />
+   <dpdhl-icon width={args.width} height={args.height} icon="file_pdf" />
+   <dpdhl-icon width={args.width} height={args.height} icon="copy" />
+   <dpdhl-icon width={args.width} height={args.height} icon="public_sector" />
+   <dpdhl-icon width={args.width} height={args.height} icon="consumer" />
 </Template>
 
 <style>
    
 </style>
 
-<Story name="Primary" args={{}}/>
+<Story name="Primary" args={{
+    width:  64,
+    height: 64,
+}}/>
--- uilib/uilib/src/components/dpdhl-icon/dpdhl-icon.svelte
+++ uilib/uilib/src/components/dpdhl-icon/dpdhl-icon.svelte
@@ -1,28 +1,37 @@
-<script>
+<script lang="ts">
     import * as icons from '../../icons';
+
+    // SVG Masks
+    // https://stackoverflow.com/questions/22252472/how-to-change-the-color-of-an-svg-element
 
     export let width = 16
     export let height = 16
     export let name = ''
-    export let color = 'black'
     export let icon = '';
 
-    // export let color = 'currentColor'
+    $: style = `
+        width:  ${width}px;
+        height: ${height}px;
+    `
+    $: _name = name ? name : icon
+
+    $: if( icon !== "" && !Boolean(icons[icon]) ){
+        console.warn(`msg="icon not found" icon="${icon}"`)
+    }
+
 </script>
 <svelte:options tag="dpdhl-icon" />
 
-{#if icon !== ""}
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  width={width}
-  height={height}
-  viewBox="0 0 48 48"
-  aria-labelledby={name}
-  role="presentation"
->
-    <title id={name} lang="en">{`${name} icon`}</title>
-        <g stroke={color} fill={color}>
-            <path d={icons[icon]}/>
-        </g>
-</svg>
-{/if}
+<img 
+    {style}
+    src={icons[icon]} 
+    alt={_name}  
+/>
+
+
+<style>
+    :host{
+        display:     grid;
+        place-items: center;
+    }
+</style>
