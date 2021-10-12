0a5bc3dfd9cb2b0e2aeb5e2dd53aa23aae3b09a7
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-10-12 11:09:23 +0200

    add checkbox component

---
+++ design-system/uilib/src/components/dpdhl-checkbox/dpdhl-checkbox.stories.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+    import '@sprinteins/dpdhl-uilib'
+</script>
+  
+<Meta title="11_Components/Checkbox" component={null}/>
+
+<Template let:args>
+</Template>
+
+<style>
+      main {
+   
+      }
+
+</style>
+
+<Story name="Primary" args={{disabled: false, indeterminate: false, error: false}} let:args>
+      <main>
+            <dpdhl-checkbox label="Label">    
+            <span>&nbsp;</span>
+      </main>
+</Story>
+
+    <!-- disabled={args.disabled} 
+                  indeterminate={args.indeterminate} 
+                  error={args.error}  -->
---
+++ design-system/uilib/src/components/dpdhl-checkbox/dpdhl-checkbox.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+
+    /**
+      * A Checkbox Component
+      * @component
+    */
+
+    export let value 
+
+    export let label = ""
+    $: _label = label
+
+    export let name = ""
+    $: _name = name
+
+    export let checked = false
+    $: _checked = checked
+
+    export let indeterminate = false
+    $: _indeterminate = indeterminate
+
+    export let disabled = false
+    $: _disabled = disabled
+
+    export let error = false
+    $: _error = error
+    
+</script>
+<svelte:options tag={'dpdhl-checkbox'}/>
+
+
+<span>
+    <label>
+        <input
+        value={value}
+        id={_name} 
+        name={_name}
+        type="checkbox"
+        bind:checked={_checked}
+        on:click 
+        disabled={_disabled}
+        indeterminate={_indeterminate}
+
+        class:disabled      = {_disabled}
+        class:indeterminate = {_indeterminate}
+        class:error         = { _error }>
+        {_label}
+    </label>
+</span>
+
+
+<style>
+
+
+
+
+</style>
