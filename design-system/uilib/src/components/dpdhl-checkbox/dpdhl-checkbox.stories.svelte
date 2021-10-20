53c6bda3bde5218c9adb7dbf0be5a0ae17e2d771
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-10-20 08:50:06 +0200

    remove ripple effects

--- design-system/uilib/src/components/dpdhl-checkbox/dpdhl-checkbox.stories.svelte
+++ design-system/uilib/src/components/dpdhl-checkbox/dpdhl-checkbox.stories.svelte
@@ -1,48 +1,47 @@
 <script lang="ts">
-import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
-import '@sprinteins/dpdhl-uilib'
-</script>
-  
-<Meta title="11_Components/Checkbox" component={null}/>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import "@sprinteins/dpdhl-uilib";
 
-<Template let:args>
-</Template>
+	$: checked = false;
+</script>
+
+<Meta title="11_Components/Checkbox" component={null} />
+
+<Template let:args />
+
+<Story
+  name="Primary"
+  args={{
+    disabled: false,
+    indeterminate: false,
+    error: false,
+    withLabel: true,
+    label: "Label",
+  }}
+  let:args
+>
+  <main>
+    <dpdhl-checkbox
+      name="my-checkbox"
+      disabled={args.disabled}
+      checked={checked}
+      indeterminate={args.indeterminate}
+      error={args.error}
+    >
+      {#if args.withLabel}
+        <span slot="label" class="label">{args.label}</span>
+      {/if}
+    </dpdhl-checkbox>
+		<div class="checked">Checked: {checked}</div>
+  </main>
+</Story>
 
 <style>
-	main {
-		display:               grid; 
-		grid-template-columns: 1fr 1fr 1fr 1fr 1fr; 
-		grid-template-rows:    1fr 1fr 1fr 1fr 1fr; 
-		gap: 1rem; 
+  .label {
+    margin-left: 0.85rem;
+    margin-top: 0.36rem;
+  }
+	.checked {
+		margin-top: 1rem;
 	}
-	.label {
-		margin-left: 0.85em;
-		margin-top: 0.36em;
-	}
-
 </style>
-
-<Story 
-	name="Primary" 
-	args={{
-      	disabled: false, 
-      	indeterminate: false, 
-      	error: false, 
-      	checked: false,
-		withLabel: true,
-	  	label: "Label"
-	}} 
-	let:args>
-      	<main>
-            <dpdhl-checkbox 
-            name="my-checkbox" 
-            disabled={args.disabled}
-            checked={args.checked}
-            indeterminate={args.indeterminate}
-            error={args.error}>    
-				{#if args.withLabel} 
-					<span slot="label" class="label">{args.label}</span>
-				{/if}
-			</dpdhl-checkbox>
-      	</main>
-</Story>
--- design-system/uilib/src/components/dpdhl-checkbox/dpdhl-checkbox.svelte
+++ design-system/uilib/src/components/dpdhl-checkbox/dpdhl-checkbox.svelte
@@ -27,204 +27,66 @@
   $: _error = error;
 </script>
 
-
-<label class="checkbox coloured">
-	<input 
-		{value}
-		id={_name}
-		name={_name}
-		type="checkbox"
-		bind:checked={_checked}
-		on:click
-		disabled={_disabled}
-		indeterminate={_indeterminate}/>
-	<span class="box" >
-		<span 
-		class="check" 
-		class:error={_error} 		
-		class:disabled={_disabled}
-		class:indeterminate={_indeterminate}/>
-	</span>
-	<slot name="label" />
-</label>
-
+<span>
+  <label>
+    <input
+      {value}
+      id={_name}
+      name={_name}
+      type="checkbox"
+      bind:checked={_checked}
+      on:click
+      disabled={_disabled}
+      indeterminate={_indeterminate}
+      class:disabled={_disabled}
+      class:indeterminate={_indeterminate}
+      class:error={_error}
+    />
+    {_label}
+  </label>
+</span>
 
 <style>
-
-  /* Hide native checkbox */
   input[type="checkbox"] {
-    opacity: 0;
-    position: absolute;
-    margin: 0;
-    z-index: -1;
-    width: 0;
-    height: 0;
-    overflow: hidden;
-    left: 0;
-    pointer-events: none;
-  }
-
-  .checkbox {
-    display: flex;
-    transform: translateZ(0);
-		z-index: 1500;
-		position: absolute;
-  }
-
-  .box {
-    vertical-align: middle;
     position: relative;
-    top: 0.1875em;
+    cursor: pointer;
   }
-  .box:before {
-    position: absolute;
-    left: 0.5em;
-    top: 0.5em;
+  input[type="checkbox"]:before {
     content: "";
-    background-color: rgba(0, 0, 0, 0.5);
-    height: 0.25em;
-    width: 0.25em;
-    border-radius: 100%;
-    z-index: 1;
-    opacity: 0;
-    margin: 0;
-  }
-  .check {
-    position: relative;
-    display: inline-block;
-    width: 1.25em;
-    height: 1.25em;
-    border: 1px solid var(--color-gray50);
-    border-radius: 3px;
-    overflow: hidden;
-    z-index: 1;
-  }
-	.error {
-		border: 1px solid var(--color-dhlred);
-		background: var(--color-dhlred-light);
-  }
-  .check:before {
-    position: absolute;
-    content: "";
-    transform: rotate(45deg);
     display: block;
-		margin-top: -0.125em;
-    margin-left: 0.5em;
-    width: 0;
-    height: 0;
-    animation: checkbox-off var(--checkbox-ripple-length) forwards ease-out;
+    position: absolute;
+    width: 1rem;
+    height: 1rem;
+    top: 0;
+    left: 0;
+    border: 1px solid var(--color-gray45);
+    border-radius: 3px;
+    background-color: white;
   }
-  input[type="checkbox"]:focus + .box .check:after {
-    opacity: 0.2;
-  }
-  input[type="checkbox"]:checked + .box .check:before {
-    box-shadow: 0 0 0 10px, 10px -10px 0 10px, 32px 0px 0 20px, 0px 32px 0 20px,
-      -5px 5px 0 10px, 20px -12px 0 11px;
-    animation: checkbox-on var(--checkbox-ripple-length) forwards ease-out;
-  }
-
-  /* Ripple effect on click */
-  input[type="checkbox"]:not(:checked) + .box:before,
-  input[type="checkbox"]:not(:checked) + .box .check:after {
-    animation: rippleOff var(--checkbox-animation-length) forwards ease-out;
-  }
-  input[type="checkbox"]:checked + .box:before,
-  input[type="checkbox"]:checked + .box .check:after {
-    animation: rippleOn var(--checkbox-animation-length) forwards ease-out;
-  }
-
-  /* Style for disabled inputs */
-  input[type="checkbox"][disabled]:not(:checked) ~ .box .check:before {
-		background-color: var(--color-gray20);
-		cursor: not-allowed;
-		opacity: 0.7;
-  }
-  input[type="checkbox"][disabled] + .box .check:after {
-    background-color: rgba(0, 0, 0, 0.84);
-    transform: rotate(-45deg);
-  }
-
-  /* Default style for input (not disabled/error) */
-  .coloured .box .check {
-    color: var(--color-postyellow);
-  }
-
-  .coloured .box:before {
+  input[type="checkbox"]:checked:before {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 1rem;
+    height: 1rem;
+    top: 0;
+    left: 0;
+    border: 1px solid var(--color-postyellow);
+    border-radius: 3px;
     background-color: var(--color-postyellow);
   }
-  .coloured input[type="checkbox"]:checked + .box .check {
-    color: var(--color-postyellow);
-		border: 1px solid var(--color-postyellow);
-  }
-
-  /* Animations */
-  @keyframes checkbox-on {
-    0% {
-      box-shadow: 0 0 0 10px, 10px -10px 0 10px, 32px 0px 0 20px,
-        0px 32px 0 20px, -5px 5px 0 10px, 15px 2px 0 11px;
-    }
-    50% {
-      box-shadow: 0 0 0 10px, 10px -10px 0 10px, 32px 0px 0 20px,
-        0px 32px 0 20px, -5px 5px 0 10px, 20px 2px 0 11px;
-    }
-    100% {
-      box-shadow: 0 0 0 10px, 10px -10px 0 10px, 32px 0px 0 20px,
-        0px 32px 0 20px, -5px 5px 0 10px, 20px -12px 0 11px;
-    }
-  }
-  @keyframes checkbox-off {
-    0% {
-      box-shadow: 0 0 0 10px, 10px -10px 0 10px, 32px 0px 0 20px,
-        0px 32px 0 20px, -5px 5px 0 10px, 20px -12px 0 11px, 0 0 0 0 inset;
-    }
-    25% {
-      box-shadow: 0 0 0 10px, 10px -10px 0 10px, 32px 0px 0 20px,
-        0px 32px 0 20px, -5px 5px 0 10px, 20px -12px 0 11px, 0 0 0 0 inset;
-    }
-    50% {
-      transform: rotate(45deg);
-      margin-top: -0.25em;
-      margin-left: 0.375em;
-      width: 0;
-      height: 0;
-      box-shadow: 0 0 0 10px, 10px -10px 0 10px, 32px 0px 0 20px,
-        0px 32px 0 20px, -5px 5px 0 10px, 15px 2px 0 11px, 0 0 0 0 inset;
-    }
-    51% {
-      transform: rotate(0deg);
-      margin-top: -0.125em;
-      margin-left: -0.125em;
-      width: 1.5em;
-      height: 1.5em;
-      box-shadow: 0 0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 0,
-        0px 0px 0 10px inset;
-    }
-    100% {
-      transform: rotate(0deg);
-      margin-top: -0.125em;
-      margin-left: -0.125em;
-      width: 1.5em;
-      height: 1.5em;
-      box-shadow: 0 0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 0, 0 0 0 0,
-        0px 0px 0 0px inset;
-    }
-  }
-  @keyframes rippleOn {
-    0% {
-      opacity: 0.5;
-    }
-    100% {
-      opacity: 0;
-      transform: scale(13, 13);
-    }
-  }
-  @keyframes rippleOff {
-    0% {
-      opacity: 0.5;
-    }
-    100% {
-      opacity: 0;
-      transform: scale(13, 13);
-    }
+  input[type="checkbox"]:checked:after {
+    content: "";
+    display: block;
+    width: 5px;
+    height: 10px;
+    border: solid black;
+    border-width: 0 1px 1px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+    position: absolute;
+    top: 2px;
+    left: 6px;
   }
 </style>
