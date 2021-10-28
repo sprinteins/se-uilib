7d2631e9a7f7e41495d1b8f8be31766fe2e8735e
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-10-28 09:08:06 +0200

    Revert "add autocomplete component"
    This reverts commit 80d34772b87dbb122107a4ff5f72ec0608660a8e.

--- design-system/uilib/src/components/dpdhl-autocomplete/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as DPDHLAutocomplete } from './dpdhl-autocomplete-item.svelte'
-export { default as DPDHLAutocompleteItem } from './dpdhl-autocomplete-item.svelte'
--- design-system/uilib/src/components/dpdhl-autocomplete/item.ts
+++
@@ -1,4 +0,0 @@
-export interface Item {
-    label: string
-    value: unknown
-}
