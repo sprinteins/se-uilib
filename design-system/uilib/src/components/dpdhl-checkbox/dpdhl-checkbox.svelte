0e5560f54f39b9bd2b6a7ff71b6989478fa1c4c6
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-11-04 09:30:15 +0100

    add tabulation of css

--- design-system/uilib/src/components/dpdhl-checkbox/dpdhl-checkbox.svelte
+++ design-system/uilib/src/components/dpdhl-checkbox/dpdhl-checkbox.svelte
@@ -60,127 +60,127 @@
 
 <style>
 	input {
-		position: relative;
-		cursor: pointer;
-		width: 1.5rem;
-		height: 1.5rem;
+		position: 		relative;
+		cursor: 		pointer;
+		width: 			1.5rem;
+		height: 		1.5rem;
 		-moz-appearance:none;
 	}
 
 	/* box */
 	input:before {
-		content: "";
-		display: block;
-		position: absolute;
-		width: 1.5rem;
-		height: 1.5rem;
-		top: 0;
-		left: 0;
-		border: 1px solid var(--color-gray45);
-		border-radius: 3px;
-		background-color: white;
+		content: 			"";
+		display: 			block;
+		position: 			absolute;
+		width: 				1.5rem;
+		height: 			1.5rem;
+		top: 				0;
+		left: 				0;
+		border: 			1px solid var(--color-gray45);
+		border-radius: 		3px;
+		background-color: 	white;
 	}
 
 	/* box checked */
 	input:checked:before {
-		border: 1px solid var(--color-postyellow);
-		background-color: var(--color-postyellow);
+		border: 			1px solid var(--color-postyellow);
+		background-color: 	var(--color-postyellow);
 	}
 	/* check */
 	input:checked:after {
-		content: "";
-		display: block;
-		width: 0.4375rem;
-		height: 0.625rem;
-		border: solid var(--color-gray60);
-		border-width: 0 1px 1px 0;
-		-webkit-transform: rotate(45deg);
-		-ms-transform: rotate(45deg);
-		transform: rotate(45deg);
-		position: absolute;
-		top: 0.3125rem;
-		left: 0.5625rem;
+		content: 			"";
+		display: 			block;
+		width: 				0.4375rem;
+		height: 			0.625rem;
+		border: 			solid var(--color-gray60);
+		border-width: 		0 1px 1px 0;
+		-webkit-transform: 	rotate(45deg);
+		-ms-transform: 		rotate(45deg);
+		transform: 			rotate(45deg);
+		position: 			absolute;
+		top: 				0.3125rem;
+		left: 				0.5625rem;
 	}
 
 	/* box disabled */
 	input.disabled:not(:checked):before {
-		background-color: var(--color-gray10);
-		cursor: not-allowed;
-		border: 1px solid var(--color-gray10);
-		color: grey;
+		background-color: 	var(--color-gray10);
+		cursor: 			not-allowed;
+		border: 			1px solid var(--color-gray10);
+		color: 				grey;
 	}
 	/* box checked disabled */
 	input.disabled:checked:before {
-		background-color: var(--color-gray10);
-		cursor: not-allowed;
-		color: grey;
-		border: 1px solid var(--color-gray10);
+		background-color: 	var(--color-gray10);
+		cursor: 			not-allowed;
+		color: 				grey;
+		border: 			1px solid var(--color-gray10);
 	}
 	/* check disabled */
 		input.disabled:checked:after {
-		cursor: not-allowed;
-		content: "";
-		display: block;
-		width: 0.4375rem;
-		height: 0.625rem;
-		border: solid var(--color-gray60);
-		border-width: 0 1px 1px 0;
-		-webkit-transform: rotate(45deg);
-		-ms-transform: rotate(45deg);
-		transform: rotate(45deg);
-		position: absolute;
-		top: 0.3125rem;
-		left: 0.5625rem;
+		cursor: 			not-allowed;
+		content: 			"";
+		display: 			block;
+		width: 				0.4375rem;
+		height: 			0.625rem;
+		border: 			solid var(--color-gray60);
+		border-width: 		0 1px 1px 0;
+		-webkit-transform: 	rotate(45deg);
+		-ms-transform: 		rotate(45deg);
+		transform: 			rotate(45deg);
+		position: 			absolute;
+		top: 				0.3125rem;
+		left: 				0.5625rem;
 	}
 
 	input.indeterminate:before {
-		border: 1px solid var(--color-postyellow);
-		background-color: var(--color-postyellow);
+		border: 			1px solid var(--color-postyellow);
+		background-color: 	var(--color-postyellow);
 	}
 
 	/* indeterminate  */
 	input.indeterminate:after {
-		content: "";
-		display: block;
-		width: 0.5rem;
-		height: 0.2rem;
-		border: solid var(--color-white);
-		border-width: 1px 0 0 0;
-		position: absolute;
-		top: 0.8rem;
-		left: 0.5625rem;
+		content: 		"";
+		display: 		block;
+		width: 			0.5rem;
+		height: 		0.2rem;
+		border: 		solid var(--color-white);
+		border-width: 	1px 0 0 0;
+		position: 		absolute;
+		top: 			0.8rem;
+		left: 			0.5625rem;
 	}
 
 	/* indeterminate  */
 	input.indeterminate.error:after {
-		content: "";
-		display: block;
-		width: 0.5rem;
-		height: 0.2rem;
-		border: solid var(--color-dhlred);
-		border-width: 1px 0 0 0;
-		position: absolute;
-		top: 0.8rem;
-		left: 0.5625rem;
+		content: 		"";
+		display: 		block;
+		width: 			0.5rem;
+		height: 		0.2rem;
+		border: 		solid var(--color-dhlred);
+		border-width: 	1px 0 0 0;
+		position: 		absolute;
+		top: 			0.8rem;
+		left: 			0.5625rem;
 	}
 
 	/* error */
 	input.error:before {
-		border: 1px solid var(--color-dhlred);
-		background-color: var(--color-dhlred-light);
+		border: 			1px solid var(--color-dhlred);
+		background-color: 	var(--color-dhlred-light);
 	}
 	input.error:checked:after {
-		content: "";
-		display: block;
-		width: 0.4375rem;
-		height: 0.6rem;
-		border: solid var(--color-dhlred);
-		border-width: 0 1px 1px 0;
-		-webkit-transform: rotate(45deg);
-		-ms-transform: rotate(45deg);
-		transform: rotate(45deg);
-		position: absolute;
-		top: 0.3125rem;
-		left: 0.5625rem;
+		content: 		"";
+		display: 		block;
+		width: 			0.4375rem;
+		height: 		0.6rem;
+		border: 		solid var(--color-dhlred);
+		border-width: 	0 1px 1px 0;
+		-webkit-transform: 	rotate(45deg);
+		-ms-transform: 		rotate(45deg);
+		transform: 			rotate(45deg);
+		position: 			absolute;
+		top: 				0.3125rem;
+		left: 				0.5625rem;
 	}
 </style>
