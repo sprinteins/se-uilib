a3f6ec7e835d2b6d5a578b9e383bef5d51bdf524
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-11-04 11:46:39 +0100

    transform pixels to rems

--- design-system/uilib/src/components/dpdhl-toggle/dpdhl-toggle.svelte
+++ design-system/uilib/src/components/dpdhl-toggle/dpdhl-toggle.svelte
@@ -47,46 +47,45 @@
 
 <style>
 	.switch {
-		position: relative;
-		display: inline-block;
-		width: 34px;
-		height: 14px;
-		margin: 0.375rem;
+		position: 	relative;
+		display: 	inline-block;
+		width: 		2.125rem;
+		height: 	0.875rem;
+		margin: 	0.375rem;
 	}
 
 	/* Hide default HTML checkbox */
 	.switch input {
-		opacity: 0;
-		width: 0;
-		height: 0;
+		opacity: 	0;
+		width: 		0;
+		height: 	0;
 	} 
 
 	.slider {
-		position: absolute;
-		cursor: pointer;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		background-color: var(--color-gray30);
+		position: 			absolute;
+		cursor: 			pointer;
+		top: 				0;
+		left: 				0;
+		right: 				0;
+		bottom: 			0;
+		background-color: 	var(--color-gray30);
 		-webkit-transition: .4s;
-		transition: .4s;
-		border-radius: 34px;
+		transition: 		.4s;
+		border-radius: 		2.125rem;
 	}
 
 	.slider:before {
-		position: absolute;
-		content: "";
-		height: 20px;
-		width: 20px;
-		left: -2px;
-		bottom: -2px;
-		background-color: white;
+		position: 			absolute;
+		content: 			"";
+		height: 			1.25rem;
+		width: 				1.25rem;
+		left: 				-0.125rem;
+		bottom: 			-0.125rem;
+		background-color: 	white;
 		-webkit-transition: .4s;
-		transition: .4s;
-		border-radius: 50%;
-		box-shadow: 0 3px 5px rgba(0,0,0,0.12), 0 3px 5px rgba(0,0,0,0.24);
-		z-index: 100;
+		transition: 		.4s;
+		border-radius: 		50%;
+		box-shadow: 		0 3px 5px rgba(0,0,0,0.12), 0 3px 5px rgba(0,0,0,0.24);
 	}
 
 	input:checked + .slider {
@@ -98,9 +97,9 @@
 	}
 
 	input:checked + .slider:before {
-		-webkit-transform: translateX(20px);
-		-ms-transform: translateX(20px);
-		transform: translateX(20px);
+		-webkit-transform: 	translateX(1.3125rem);
+		-ms-transform: 		translateX(1.3125rem);
+		transform: 			translateX(1.3125rem);
 	}
 
 
