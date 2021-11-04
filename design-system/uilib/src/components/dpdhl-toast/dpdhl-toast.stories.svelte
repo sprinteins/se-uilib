df7e2547a01edf5794a2311d90f425c6c8071204
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-11-04 10:15:17 +0100

    improve indentation in toast

--- design-system/uilib/src/components/dpdhl-toast/dpdhl-toast.stories.svelte
+++ design-system/uilib/src/components/dpdhl-toast/dpdhl-toast.stories.svelte
@@ -1,63 +1,62 @@
 <script>
-  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
-  import "@sprinteins/dpdhl-uilib";
-  import { Type } from "./type";
+	import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+	import "@sprinteins/dpdhl-uilib";
+	import { Type } from "./type";
 
-  let open = true;
+	let open = true;
 
-  function handleClose() {
-    open = false;
-  }
+	function handleClose() {
+		open = false;
+	}
 
-	  function handleOpen() {
-    open = true;
-  }
+	function handleOpen() {
+		open = true;
+	}
 
 </script>
 
 <!-- https://www.dpdhl-brands.com/dpdhl-group/en/guides/touchpoints/user-interface/components/notifications.html -->
 <Template let:args>
-  <dpdhl-toast
-    {open}
-    on:closeToast={handleClose}
-    type="error"
-    title="Toast Title"
-    message="And here we go with a more detailed message."
-    {...args}
-  >
-  {#if args.showIcon}
-    <dpdhl-icon
-      slot="icon"
-      width={20}
-      height={20}
-      color="#FFF"
-      icon="exception"
-    />
-    {/if}
-  </dpdhl-toast>
+	<dpdhl-toast
+		{open}
+		on:closeToast={handleClose}
+		type="error"
+		title="Toast Title"
+		message="And here we go with a more detailed message."
+		{...args}
+	>
+		{#if args.showIcon}
+			<dpdhl-icon
+			slot="icon"
+			width={20}
+			height={20}
+			color="#FFF"
+			icon="exception"
+			/>
+		{/if}
+	</dpdhl-toast>
 	<button on:click={() => handleOpen()}>Show toast</button>
 </Template>
 
 <Meta
-  title="11_Components/Toast"
-  component={null}
-  argTypes={{
-    type: {
-      options: [Type.error, Type.notification],
-      control: { type: "select" },
-    },
-  }}
+	title="11_Components/Toast"
+	component={null}
+	argTypes={{
+		type: {
+			options: [Type.error, Type.notification],
+			control: { type: "select" },
+		},
+	}}
 />
 
 <Story
-  name="Primary"
-  args={{
-    type: Type.error,
-    title: "Toast Title",
-    message: "And here we go with a more detailed message.",
-    showIcon: false,
-  }}
+	name="Primary"
+	args={{
+		type: Type.error,
+		title: "Toast Title",
+		message: "And here we go with a more detailed message.",
+		showIcon: false,
+	}}
 />
 
-<style>
-</style>
+<style></style>
--- design-system/uilib/src/components/dpdhl-toast/dpdhl-toast.svelte
+++ design-system/uilib/src/components/dpdhl-toast/dpdhl-toast.svelte
@@ -1,133 +1,127 @@
 <svelte:options tag="dpdhl-toast" />
 
 <script lang="ts">
-  /**
-   * A Toast Component
-   * @component
-   */
+	/**
+	 * A Toast Component
+	 * @component
+	 */
 
-  /**
-   * Type of the toast: Error, Notification
-   */
-  import { Type } from "./type";
+	/**
+	 * Type of the toast: Error, Notification
+	 */
+	import { Type } from "./type";
 
-  export let open = true;
-  $: _open = open;
+	export let open = true;
+	$: _open = open;
 
-  export let message;
-  $: _message = message;
+	export let message;
+	$: _message = message;
 
-  export let type: string = Type.error;
-  export let fullWidth: boolean = true;
+	export let type: string = Type.error;
+	export let fullWidth: boolean = true;
 
-  export let title;
-  $: _title = title || "No title provided";
+	export let title;
+	$: _title = title || "No title provided";
 
-  let toast: HTMLElement;
-  function closeToast() {
-    toast.dispatchEvent(
-      new Event("closeToast", {
-        bubbles: true,
-        composed: true,
-      })
-    );
-  }
+	let toast: HTMLElement;
+	function closeToast() {
+		toast.dispatchEvent(
+			new Event("closeToast", {
+				bubbles: true,
+				composed: true,
+			})
+		);
+	}
 </script>
 
 {#if _open}
-  <main
-    class:notification={type === Type.notification}
-    class:error={type === Type.error}
-    class:full-width={fullWidth}
-    bind:this={toast}
-  >
-    <div class="content-container">
-      <!-- <span class="custom-icon" >
-            </span> -->
-      <slot name="icon" />
-
-      <div class="text-container">
-        <div class="title">
-          {_title}
-        </div>
-        {#if _message}
-          <div class="message">
-            {_message}
-          </div>
-        {/if}
-      </div>
-    </div>
-
-    <dpdhl-icon
-      on:click={closeToast}
-      width={16}
-      height={16}
-      color="#FFF"
-      icon="cancel"
-      class="close-icon"
-    />
-  </main>
+	<main
+		class:notification={type === Type.notification}
+		class:error={type === Type.error}
+		class:full-width={fullWidth}
+		bind:this={toast}
+	>
+		<div class="content-container">
+			<slot name="icon" />
+			<div class="text-container">
+				<div class="title">
+					{_title}
+				</div>
+				{#if _message}
+					<div class="message">{_message}</div>
+				{/if}
+			</div>
+		</div>
+		<dpdhl-icon
+			on:click={closeToast}
+			width={16}
+			height={16}
+			color="#FFF"
+			icon="cancel"
+			class="close-icon"
+		/>
+  	</main>
 {/if}
 
 <style>
-  main {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    padding: var(--padding-top-bottom) var(--padding-left-right);
-    padding-top: var(--toast__padding-top, 1.125rem);
-    padding-bottom: var(--toast__padding-bottom, 1.125rem);
-    padding-left: var(--toast__padding-left, 1.125rem);
-    padding-right: var(--toast__padding-right, 1.125rem);
-  }
+	main {
+		display: 			flex;
+		flex-direction: 	row;
+		align-items: 		center;
+		justify-content: 	space-between;
+		padding: 			var(--padding-top-bottom) var(--padding-left-right);
+		padding-top: 		var(--toast__padding-top, 1.125rem);
+		padding-bottom: 	var(--toast__padding-bottom, 1.125rem);
+		padding-left: 		var(--toast__padding-left, 1.125rem);
+		padding-right: 		var(--toast__padding-right, 1.125rem);
+	}
 
-  main.full-width {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    flex-grow: 1;
-  }
+	main.full-width {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		flex-grow: 1;
+	}
 
-  main.notification {
-    color: var(--toast__color, var(--color-white));
-    background-color: var(--color-green-dark);
-  }
+	main.notification {
+		color: 				var(--toast__color, var(--color-white));
+		background-color: 	var(--color-green-dark);
+	}
 
-  main.error {
-    color: var(--toast__color, var(--color-white));
-    background-color: var(--color-dhlred);
-  }
+	main.error {
+		color: 				var(--toast__color, var(--color-white));
+		background-color: 	var(--color-dhlred);
+	}
 
-  .title {
-    font-size: var(--font-size);
-    line-height: var(--line-height);
-    font-weight: 700;
-    padding: 0;
-    margin: 0;
-  }
+	.title {
+		font-size: 		var(--font-size);
+		line-height: 	var(--line-height);
+		font-weight: 	700;
+		padding: 		0;
+		margin: 		0;
+	}
 
-  .message {
-    font-size: calc(var(--font-size) * 0.875);
-    line-height: var(--line-height);
-    font-weight: 400;
-    padding: 0;
-    margin: 0;
-  }
+	.message {
+		font-size: 		calc(var(--font-size) * 0.875);
+		line-height: 	var(--line-height);
+		font-weight: 	400;
+		padding: 		0;
+		margin: 		0;
+	}
 
-  div.text-container {
-    display: flex;
-    flex-direction: column;
-  }
+	div.text-container {
+		display: 		flex;
+		flex-direction: column;
+	}
 
-  div.content-container {
-    display: flex;
-    flex-direction: row;
-    gap: 0.75rem;
-  }
+	div.content-container {
+		display: 		flex;
+		flex-direction: row;
+		gap: 			0.75rem;
+	}
 
-  .close-icon {
-    cursor: pointer;
-  }
+	.close-icon {
+		cursor: 		pointer;
+	}
 </style>
