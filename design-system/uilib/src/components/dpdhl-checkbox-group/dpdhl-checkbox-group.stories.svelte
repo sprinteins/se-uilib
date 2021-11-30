fb9f8272f4f27db21c6c6b7c4940f6de2f5f75f0
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-11-30 12:54:27 +0100

    display selected items in story + cleanup

--- design-system/uilib/src/components/dpdhl-checkbox-group/dpdhl-checkbox-group.stories.svelte
+++ design-system/uilib/src/components/dpdhl-checkbox-group/dpdhl-checkbox-group.stories.svelte
@@ -7,8 +7,7 @@
     let selected = []
 
     function handleSelectItems(event) {
-        console.log(event.detail)
-        selected = event.detail.selected
+        selected = event.detail;
     }
 </script>
 
@@ -16,23 +15,17 @@
 <Meta title="11_Components/Checkbox Group" component={null}/>
 
 <Template let:args>
-    <div class="container">
+    <div>
         <DPDHLCheckboxGroup on:select={handleSelectItems}>
             <DPDHLCheckboxItem value="option1">Menu 1 - a long one</DPDHLCheckboxItem>
             <DPDHLCheckboxItem value="option2">Menu 2</DPDHLCheckboxItem>
             <DPDHLCheckboxItem value="option3">Menu 3</DPDHLCheckboxItem>
         </DPDHLCheckboxGroup>
     </div>
-
-    <p>Selected Items: {selected.join(', ')} </p>
-
+    <p>Selected Items: {selected.length ? selected.join(', ') : ""} </p>
 </Template>
 
 
 <Story name="Primary" args={{}}/>
 
-<style>
-    .container {
-        padding-left: 1rem;
-    }
-</style>
+<style></style>
