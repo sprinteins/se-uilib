9351e6b8666db1adfe761cc937c8ef1eb4c087ae
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-11-30 10:10:22 +0100

    add more components to svelte example app

--- example-apps/app-svelte/src/App.svelte
+++ example-apps/app-svelte/src/App.svelte
@@ -2,27 +2,48 @@
 	
 	import '@sprinteins/dpdhl-uilib'
 
+	let items = [
+        { label:'Menu 1', click: () => alert('Menu 1') },
+        { label:'Menu 2', click: () => alert('Menu 2') },
+        { label:'Menu 3', click: () => alert('Menu 3') },		
+    ]
+
 </script>
 
 <main>
 		<dpdhl-theme-desktop>
-			<dpdhl-button type="primary">in theme DPDHL</dpdhl-button>
+			<div class="container">
+				<dpdhl-button type="primary">Button</dpdhl-button>
+				<dpdhl-menu items={items} />
+				<dpdhl-card>  
+						<h3 slot="header">
+							Card Header
+						</h3>
+						<div slot="content">
+							Amet nulla facilisi morbi tempus iaculis 
+							urna id volutpat lacus. Sed faucibus turpis in 
+							eu mi bibendum. Amet consectetur adipiscing elit 
+							pellentesque habitant morbi tristique.
+						</div>
+						<div slot="footer" class="footer" >
+							<dpdhl-button type="text"> Main Action </dpdhl-button>
+						</div>
+				</dpdhl-card>
+			</div>
 		</dpdhl-theme-desktop>
 </main>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.container {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
 	}
 
 	@media (min-width: 640px) {
