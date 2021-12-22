54e8347df63d264be576f29d672d698c853e9cee
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-12-22 12:28:22 +0100

    rewrite the story of checkbox group

--- design-system/uilib/src/components/dpdhl-checkbox-group/dpdhl-checkbox-group.stories.svelte
+++ design-system/uilib/src/components/dpdhl-checkbox-group/dpdhl-checkbox-group.stories.svelte
@@ -1,8 +1,7 @@
-
 <script>
     import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+    
     import '@sprinteins/dpdhl-uilib'
-    import { DPDHLCheckboxGroup, DPDHLCheckboxItem } from './index'
 
     let selected = []
 
@@ -12,29 +11,28 @@
 </script>
 
 
-<Meta 
-title="11_Components/Checkbox Group" 
-component={null}
-argTypes={{
+<Meta title="11_Components/Checkbox Group" component={null} argTypes={{
     itemsGap: {
       name: "Gap between items [rem]", 
     },
 }}/>
 
+<!-- https://www.dpdhl-brands.com/dpdhl-group/en/guides/touchpoints/user-interface/components/select.html -->
+<Template let:args>
+</Template>
+
+
 <Template let:args>
     <div style="--checkbox-group__items-gap: {args.itemsGap}rem;">
-        <DPDHLCheckboxGroup on:select={handleSelectItems}>
-            <DPDHLCheckboxItem value="option1">Option 1</DPDHLCheckboxItem>
-            <DPDHLCheckboxItem value="option2">Option 2</DPDHLCheckboxItem>
-            <DPDHLCheckboxItem value="option3" disabled={true}>Disabled option</DPDHLCheckboxItem>
-            <DPDHLCheckboxItem value="option4" indeterminate={true}>Indeterminate option</DPDHLCheckboxItem>
-            <DPDHLCheckboxItem value="option5" error={true}>Error option</DPDHLCheckboxItem>
-            <DPDHLCheckboxItem value="option6">Long option - This text explains the basic theory underlying core remote sensing semulacre processing operations and provides extensive hands-on exercises using propositional logic.</DPDHLCheckboxItem>
-        </DPDHLCheckboxGroup>
+        <dpdhl-checkbox-group placeholder="Select field" on:select={handleSelectItems}>
+            <dpdhl-checkbox-item label="Option A" value="option-a" />
+            <dpdhl-checkbox-item label="Option B" value="option-b" disabled={true}/>
+            <dpdhl-checkbox-item label="Option C" value="option-c" error={true}/>
+            <dpdhl-checkbox-item label="Option C" value="option-c" error={true}/>
+        </dpdhl-checkbox-group>
     </div>
     <p>Selected Items: {selected.length && selected.join(', ')} </p>
 </Template>
-
 
 <Story name="Primary" args={{
     itemsGap: 1.5
