84d650c2129a9a4084f204c8704f5c87be9ca879
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-12-22 11:47:20 +0100

    remove context from checkbox group

--- design-system/uilib/src/components/dpdhl-checkbox-group/dpdhl-checkbox-group.svelte
+++ design-system/uilib/src/components/dpdhl-checkbox-group/dpdhl-checkbox-group.svelte
@@ -1,66 +1,117 @@
-<script context="module" lang="ts">
-	export const ITEMS = {};
-</script>
-
 <script lang="ts">
-	import { setContext, onDestroy } from 'svelte';
-	import { writable } from 'svelte/store';
-	import { createEventDispatcher } from 'svelte';
-	import { get_current_component } from "svelte/internal";
-
-	const items = [];
-	let selectedItems = writable([]);
-
-	const component = get_current_component()
-	const svelteDispatch = createEventDispatcher()
-
-	export function dispatch(name, detail = null) {
-		svelteDispatch(name, detail)
-		component.dispatchEvent && component.dispatchEvent(new CustomEvent(name, { detail }))
-	}
-
-    function handleSelect(items) {
-        dispatch('select', items)
+    import { onMount } from 'svelte'
+	import { makeEvent } from '../../x/util/dispatch'
+    import "../dpdhl-icon"
+    import { KeyItemAdded } from './dpdhl-checkbox-item.svelte'
+    
+    export let placeholder = ""
+    $: placholderItem = {
+        label: placeholder,
+        value: undefined,
+		disabled: false,
+		error: false,
+		indeterminate: false
     }
 
-	setContext(ITEMS, {
-		registerItem: item => {
-			items.push(item);
-			onDestroy(() => {
-				const i = items.indexOf(item);
-				items.splice(i, 1);
-				selectedItems.update(current => 
-					current.includes(item) 
-					? current.filter(i => i !== item)
-					: current);
-			});
-		},
-		selectItem: item => {
-            selectedItems.update(current => {
-                let selected = [...current];
-                if (current.includes(item)) {
-                    selected = selected.filter(i => i !== item)
-                } else {
-                    selected = [...selected, item]
+    interface Item {
+        label: string
+        value: unknown,
+		disabled: boolean,
+		error: boolean,
+		indeterminate: boolean
+    }
+
+    let container: HTMLElement
+    let items: Item[] = []
+	let selectedItems: Item[] = []
+    let assignedElements: HTMLElement[] = []
+
+    onMount(() => {
+        registerItems();
+        container.addEventListener(KeyItemAdded, registerItems)
+    })    
+
+    function registerItems(){
+        const slot = container.childNodes[0] as HTMLSlotElement
+
+        assignedElements = slot.assignedElements() as HTMLElement[]
+
+        assignedElements.forEach( (el, ei)  => {
+            const alreadyRegistered = el.hasAttribute('registered')
+            if( alreadyRegistered ){
+                return;
+            }
+            el.setAttribute('registered','');
+            const label = el.getAttribute('label')
+            const value = el.getAttribute('value')
+			const disabled = el.getAttribute('disabled') === "true";
+			const error = el.getAttribute('error') === "true";
+			const indeterminate = el.getAttribute('indeterminate') === "true";
+            if(items){ 
+                items[ei] = {
+                    label,
+                    value,
+					disabled,
+					error,
+					indeterminate
                 }
-                return selected;
-            })
-            handleSelect($selectedItems)
-		},
-		selectedItem: selectedItems,
-	});
+            }
+        });
+    }
+
+    let selectedItem: Item = placholderItem;
+    onMount(() => {
+        selectedItem = placholderItem;
+    })
+
+    let root: HTMLDivElement;
+    function onItemClick(item: Item) {
+		console.log(item)
+		// selectedItems.update(current => {
+        //         let selected = [...current];
+        //         if (current.includes(item)) {
+        //             selected = selected.filter(i => i !== item)
+        //         } else {
+        //             selected = [...selected, item]
+        //         }
+        //         return selected;
+        //     })
+		console.log('selected: ', selectedItems);
+		if (selectedItems.includes(item)) {
+			selectedItems = [...selectedItems.filter(i => i !== item)]
+		} else {
+			selectedItems = [...selectedItems, item]
+		}
+		console.log('selected: ', selectedItems);
+        root.dispatchEvent(makeEvent('select', selectedItems))
+    }
+
 </script>
+<svelte:options tag="dpdhl-checkbox-group" />
 
-<svelte:options tag="dpdhl-checkbox-group"/>
+<div bind:this={root} class="root">
+	{#each items as item}
+		<dpdhl-checkbox
+			on:check={() => onItemClick(item)}
+			selected={selectedItems.includes(item)}
+			value={item.value}
+			disabled={item.disabled}
+			error={item.error}>
+			{item.label}
+		</dpdhl-checkbox>
+	{/each}
+</div>
 
-<div class="container">
-	<slot></slot>
+
+<div bind:this={container}>
+    <slot />
 </div>
 
 <style>
-    .container {
+	.root {
         display:        flex;
         flex-direction: column;
-        gap:            var(--checkbox-group__items-gap, 1.5rem);
+        gap:            var(--radio-group__items-gap, 1.5rem);
     }
+
 </style>
