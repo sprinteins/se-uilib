3a3179821c6039b536c210b1b3558506ece2d9fe
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-12-23 10:25:58 +0100

    add checkbox group to angular

--- example-apps/app-angular/src/app/kitchen-sink/kitchen-sink.component.ts
+++ example-apps/app-angular/src/app/kitchen-sink/kitchen-sink.component.ts
@@ -13,6 +13,10 @@ export class KitchenSinkComponent {
         this.log = `clicked on ${buttonName}`
     }
 
+    handleCheckboxSelect(event) {
+        alert(event.detail);
+    }
+
     menuItems = [
         { label: "Menu 1", click: () => { alert('You have selected "Menu 1"') }},
         { label: "Menu 2", click: () => { alert('You have selected "Menu 2"') }},
