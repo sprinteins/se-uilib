0c239123850b152dd2c6467267df4667a915816a
Daria Vodoleeva <d.vodoleeva@sprinteins.com>
2021-12-30 10:42:09 +0100

    add defaultId to args

--- design-system/uilib/src/components/dpdhl-segmented-control/dpdhl-segmented-control.stories.svelte
+++ design-system/uilib/src/components/dpdhl-segmented-control/dpdhl-segmented-control.stories.svelte
@@ -6,20 +6,20 @@
     let selected = "";
 </script>
 
-
 <Meta title="11_Components/Segmented Control" component={null} />
 
-<Template let:args>
-</Template>
+<Template let:args/>
 
-
-<Story name="Primary" args={{}}>
-    <dpdhl-segmented-control defaultid={1} on:select={(event) => selected = event.detail}>
-        <dpdhl-segmented-control-item label="Option A" value="option-a" />
-        <dpdhl-segmented-control-item label="Option B" value="option-b" disabled={true}/>
-        <dpdhl-segmented-control-item label="Option C" value="option-c" error={true}/>
-    </dpdhl-segmented-control>
-    <div class="selected">Selected Id: {selected}</div>
+ <!-- TODO: passing args.defaultId as defaultid breaks the component -->
+<Story name="Primary" args={{defaultId: 0}} let:args>
+    <main>
+        <dpdhl-segmented-control defaultid={0} on:select={(event) => selected = event.detail}>
+            <dpdhl-segmented-control-item label="Option A" value="option-a" />
+            <dpdhl-segmented-control-item label="Option B" value="option-b" disabled={true}/>
+            <dpdhl-segmented-control-item label="Option C" value="option-c" error={true}/>
+        </dpdhl-segmented-control>
+        <div class="selected">Selected Id: {selected}</div>
+    </main>
 </Story>
 
 
